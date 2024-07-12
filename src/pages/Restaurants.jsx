020785616b726e37b4b0ca732b18efd51f38354c
pasha3228 <paha.petrov2015@yandex.ru>
2024-07-12 14:12:41 +0300

    Revert " НАВАЛИЛ БАЗЫ 2"
    This reverts commit cb9540700c19fe0fc73375525baf3ddbb8a9fd60.

--- src/pages/Restaurants.jsx
+++ src/pages/Restaurants.jsx
@@ -9,27 +9,18 @@ const Restaurants = ({ restaurants }) => {
   const openNextRestaurant = () => {
     setActiveRestaurant({ active: activeRestaurant.active + 1 })
   }
-  const handleRestaurantClick = (index) => {
-    setActiveRestaurant({ active: index })
-  }
+
   if (!restaurants.length) {
     return null
   }
-  const restaurantButtons = restaurants.map((restaurant, index) => (
-    <button className={styles.but} key={restaurant.id} onClick={() => handleRestaurantClick(index)}>
-      {restaurant.name}
-    </button>
-  ))
+
   return (
     <div className={styles.root}>
       <h2>Restaurants</h2>
-      <div>{restaurantButtons}</div>
       <button onClick={openNextRestaurant}>Next Restaurant</button>
-
       <div>
         <div key={restaurants[activeRestaurant.active].id}>
           <div>{restaurants[activeRestaurant.active].name}</div>
-
           <Menu menu={restaurants[activeRestaurant.active].menu} />
           <Reviews reviews={restaurants[activeRestaurant.active].reviews} />
         </div>
