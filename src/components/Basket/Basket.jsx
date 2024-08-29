f130365b71e544d4b7290ae7e64a5a0ef08c0333
Dmitriy Maximov <d.maksimov@sftpro.ru>
2024-08-29 14:39:49 +0300

    fix

---
+++ src/components/Basket/Basket.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+import styles from "./styles.module.css"
+import classnames from "classnames"
+import { BasketItemContainer } from "../../containers/BasketItem/BasketItemContainer"
+
+export const Basket = ({ productIds = [], className }) => {
+  return (
+    <div className={classnames(styles.root, className)}>
+      <span className={styles.title}>Basket</span>
+      <div className={styles.products}>
+        {productIds.map((productId) => (
+          <BasketItemContainer key={productId} productId={productId} className={styles.product} />
+        ))}
+      </div>
+      <div className={styles.finalPrice}>FinalPrice</div>
+    </div>
+  )
+}
